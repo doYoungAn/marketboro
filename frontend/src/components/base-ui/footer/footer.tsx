24ff647fb39b0a7276eaa5d86a406419f175b7b0
doYoungAn <jispoop@gmail.com>
2020-05-02 13:56:19 +0900

    ./frontend/src/components/base-ui/footer/* Footer 컴포넌트 생성

---
+++ frontend/src/components/base-ui/footer/footer.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useEffect } from 'react';
+
+interface IFooterProps {}
+
+const Footer: FC<IFooterProps> = (): JSX.Element => {
+  
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <>
+        <div>
+            Footer!
+        </div>
+    </>
+  );
+
+};
+
+export default Footer;
