69c1fb41f6d22fc179f86b4132e545357f68d4f7
doYoungAn <jispoop@gmail.com>
2020-05-02 14:16:08 +0900

    frontend uikit 적용

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import App from '~/App';
 
 import 'normalize.css';
+import 'uikit/dist/css/uikit.min.css';
+import 'uikit/dist/js/uikit.min.js';
 
 const element = document.getElementById('app');
 
