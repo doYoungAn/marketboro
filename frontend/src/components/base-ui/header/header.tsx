83fc50586924da62cf009cedba056e89d1ee0721
doYoungAn <jispoop@gmail.com>
2020-05-02 13:56:01 +0900

    ./frontend/src/components/base-ui/header/* Header 컴포넌트 생성

---
+++ frontend/src/components/base-ui/header/header.tsx
@@ -0,0 +1,21 @@
+import React, { FC, useEffect } from 'react';
+
+interface IHeaderProps {}
+
+const Header: FC<IHeaderProps> = (): JSX.Element => {
+  
+  useEffect(() => {
+
+  }, []);
+
+  return (
+    <>
+        <div>
+            Header!
+        </div>
+    </>
+  );
+
+};
+
+export default Header;
