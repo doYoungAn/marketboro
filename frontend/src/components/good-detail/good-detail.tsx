eb16f9f8d1313aea13346a841c9fc472b23e8d25
doYoungAn <jispoop@gmail.com>
2020-05-04 17:07:52 +0900

    frontend good-detail 내용 생성

--- frontend/src/components/good-detail/good-detail.tsx
+++ frontend/src/components/good-detail/good-detail.tsx
@@ -13,7 +13,9 @@ const GoodDetail: FC<IGoodDetailProps> = (): JSX.Element => {
       <div data-uk-grid>
         <div className="uk-width-1-1">
           <div className="uk-card uk-card-default uk-card-body uk-card-small">
-            상품설명 이미지칸
+            <img src="https://s3.ap-northeast-2.amazonaws.com/gred/vendor/580/images/77_1588038488.jpg" alt=""/>
+            <img src="https://s3.ap-northeast-2.amazonaws.com/gred/vendor/580/images/74_1587949088.jpg" alt=""/>
+            <img src="https://s3.ap-northeast-2.amazonaws.com/gred/vendor/580/images/84_1587949109.jpg" alt=""/>
           </div>
         </div>
       </div>
