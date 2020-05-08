dea5e2109bc13b43f294110f7fe179a1c24dd9de
doYoungAn <jispoop@gmail.com>
2020-05-08 11:33:25 +0900

    주석 제거

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from '~/App';
 
-// import 'uikit/dist/css/uikit.min.css';
-// import 'uikit/dist/js/uikit.min.js';
-// import 'uikit/dist/js/uikit-icons.min.js';
-
-// import css from 'uikit/dist/css/uikit.css';
 import Icons from 'uikit/dist/js/uikit-icons';
 import UIkit from 'uikit/dist/js/uikit.js'
 
-// loads the Icon plugin
 UIkit.use(Icons);
 
-// import 'swiper/css/swiper.min.css';
-
 import '~/services/api.service';
-
 
 const element = document.getElementById('app');
 
